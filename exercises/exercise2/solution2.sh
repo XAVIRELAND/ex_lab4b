d4ff33065dc157c738a41f2de4351bd47dffb5a3
xavireland <xavireland@gmail.com>
2019-10-01 23:32:09 +0100

    exercises_lab4b

--- exercises/exercise2/solution2.sh
+++ exercises/exercise2/solution2.sh
@@ -1,1 +1,4 @@
-# Write your script here!!! Hint: pehaps use pcap file name and MAC address as input arguements to the script...
+vagrant@webserver:~$ tshark -w /vagrant/webserver-eth1-traffic2.pcap -i eth1 
+vagrant@dbbserver:~$ ping 192.168.5.2
+C:\>ping 192.168.5.2
+vagrant@webserver:~$ tshark -r /vagrant/webserver-eth1-traffic2.pcap -Y "eth.src==08:00:27:ee:b2:19" 
